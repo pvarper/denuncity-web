7adb6ef8ad104f6a82877b9f379d2f43f9e5905d
Robert Cabrera <clrobert.06@gmail.com>
2023-05-29 19:09:46 -0400

    feat(master): Add styles

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,35 @@
+import CardFour from '../../components/CardFour';
+import CardOne from '../../components/CardOne';
+import CardThree from '../../components/CardThree';
+import CardTwo from '../../components/CardTwo';
+import ChartOne from '../../components/ChartOne';
+import ChartThree from '../../components/ChartThree';
+import ChartTwo from '../../components/ChartTwo';
+import ChatCard from '../../components/ChatCard';
+import TableOne from '../../components/TableOne';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+const ECommerce = () => {
+  return (
+    <DefaultLayout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardOne />
+        <CardTwo />
+        <CardThree />
+        <CardFour />
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartOne />
+        <ChartTwo />
+        <ChartThree />
+        <div className="col-span-12 xl:col-span-8">
+          <TableOne />
+        </div>
+        <ChatCard />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default ECommerce;
